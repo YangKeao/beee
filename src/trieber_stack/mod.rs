88109eb3717079d91dd6533b97549caa740f3c0e
Yang Keao <keao.yang@yahoo.com>
2019-03-21 17:30:36 +0800

    Support empty stack

--- src/trieber_stack/mod.rs
+++ src/trieber_stack/mod.rs
@@ -1,10 +1,11 @@
 use std::sync::atomic::AtomicPtr;
 use std::sync::atomic::Ordering;
 use std::cmp::PartialEq;
+use std::ptr::null_mut;
 
 struct Node<T: PartialEq> {
     val: T,
-    next: Option<AtomicPtr<Node<T>>>
+    next: AtomicPtr<Option<Node<T>>>
 }
 
 impl<T: PartialEq> PartialEq for Node<T> {
@@ -14,24 +15,36 @@ impl<T: PartialEq> PartialEq for Node<T> {
 }
 
 pub struct Stack<T: PartialEq> {
-    top: AtomicPtr<Node<T>>
+    top: AtomicPtr<Option<Node<T>>>
 }
 
 impl<T: PartialEq> Stack<T> {
     pub fn push(&self, val: T) {
-        let node = Box::new(Node {
+        let node = Box::new(Some(Node {
             val,
-            next: None
-        });
+            next: AtomicPtr::new(null_mut())
+        }));
         let node_ptr = Box::leak(node);
 
         loop {
             let top = self.top.load(Ordering::Relaxed);
-            node_ptr.next = Some(AtomicPtr::new(top));
+            match node_ptr {
+                Some(node) => {
+                    node.next = AtomicPtr::new(top);
+                }
+                None => {
+                    unreachable!()
+                }
+            }
 
-            if let Ok(_) = self.top.compare_exchange(top, node_ptr as *mut Node<T>, Ordering::SeqCst, Ordering::Relaxed) {
+            if let Ok(_) = self.top.compare_exchange(top, node_ptr as *mut Option<Node<T>>, Ordering::SeqCst, Ordering::Relaxed) {
                 break;
             }
+        }
+    }
+
+    pub fn pop(&self) {
+        loop {
         }
     }
 }
