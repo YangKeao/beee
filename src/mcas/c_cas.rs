8d089ae0a2d28a17f44d24aca15a16f124feb885
Yang Keao <keao.yang@yahoo.com>
2019-03-22 20:25:48 +0800

    impl CCAS operation

---
+++ src/mcas/c_cas.rs
@@ -0,0 +1,74 @@
+use std::sync::atomic::AtomicPtr;
+use std::sync::atomic::Ordering;
+use crate::mcas::Status;
+use std::sync::Arc;
+
+pub struct CCasDesc<T> {
+    inner: Arc<AtomicPtr<CCasUnion<T>>>,
+    expect: *mut CCasUnion<T>,
+    new: *mut CCasUnion<T>,
+    cond: Arc<Status>,
+}
+
+impl<T> CCasDesc<T> {
+    pub fn help(&self, desc_ptr: *mut CCasUnion<T>) {
+        let success = self.cond == Arc::new(Status::Undecided);
+        self.inner.compare_and_swap(desc_ptr, if success {self.new} else {self.expect}, Ordering::SeqCst); // TODO: set order carefully
+    }
+}
+
+pub enum CCasUnion<T> {
+    CCasDesc(CCasDesc<T>),
+    Value(T)
+}
+
+impl<T> CCasUnion<T> {
+    pub fn borrow_mut_c_cas_desc(&mut self) -> &mut CCasDesc<T> {
+        match self {
+            CCasUnion::CCasDesc(c_cas_desc) => c_cas_desc,
+            _ => unreachable!()
+        }
+    }
+}
+
+pub struct CCasPtr<T> {
+    inner: Arc<AtomicPtr<CCasUnion<T>>>
+}
+
+impl<T> CCasPtr<T> {
+    pub fn c_cas(&self, expect: *mut CCasUnion<T>, new: *mut CCasUnion<T>, cond: Arc<Status>) {
+        let mut desc = CCasUnion::CCasDesc(CCasDesc::<T> {
+            inner: self.inner.clone(),
+            expect,
+            new,
+            cond: cond.clone()
+        });
+
+        let expect_ptr = desc.borrow_mut_c_cas_desc().expect;
+        let desc_ptr = &mut desc as *mut CCasUnion<T>;
+
+        loop {
+            let res = desc.borrow_mut_c_cas_desc().inner.compare_and_swap(expect_ptr, desc_ptr, Ordering::SeqCst); // TODO: set order carefully
+            if std::ptr::eq(res, desc.borrow_mut_c_cas_desc().expect) {
+                desc.borrow_mut_c_cas_desc().help(desc_ptr);
+            } else {
+                match unsafe { &*res } {
+                    CCasUnion::CCasDesc(c_cas_desc) => c_cas_desc.help(desc_ptr),
+                    _ => return // TODO: mark failed
+                }
+            }
+        }
+    }
+
+    pub fn load(&self) -> *mut T {
+        loop {
+            unsafe {
+                let v = &mut *self.inner.load(Ordering::SeqCst) as *mut CCasUnion<T>; // TODO: set order carefully
+                match &mut *v {
+                    CCasUnion::CCasDesc(c_cas_desc) => c_cas_desc.help(v),
+                    CCasUnion::Value(val) => return val as *mut T
+                }
+            }
+        }
+    }
+}
--- src/mcas/mod.rs
+++ src/mcas/mod.rs
@@ -1,77 +1,8 @@
-use crate::utils::AtomicPtrAddOn;
-use std::cell::UnsafeCell;
-use std::cmp::Ordering;
-use std::sync::atomic::AtomicPtr;
-
-pub(crate) struct Cas<T> {
-    ptr: UnsafeCell<AtomicPtr<T>>,
-    expects: AtomicPtr<T>,
-    new: AtomicPtr<T>,
-}
-
-impl<T> Ord for Cas<T> {
-    fn cmp(&self, other: &Cas<T>) -> Ordering {
-        unsafe { self.ptr.get_addr().cmp(&other.ptr.get_addr()) }
-    }
-}
-
-impl<T> PartialOrd for Cas<T> {
-    fn partial_cmp(&self, other: &Cas<T>) -> Option<Ordering> {
-        unsafe { self.ptr.get_addr().partial_cmp(&other.ptr.get_addr()) }
-    }
-}
-
-impl<T> PartialEq for Cas<T> {
-    fn eq(&self, other: &Cas<T>) -> bool {
-        unsafe { self.ptr.get_addr() == other.ptr.get_addr() }
-    }
-}
-
-impl<T> Eq for Cas<T> {}
-
-enum Status {
+#[derive(PartialEq)]
+pub enum Status {
     Undecided,
     Failed,
     Successful,
 }
 
-struct MCasDesc<T> {
-    cases: Vec<Cas<T>>,
-    status: Box<Status>,
-}
-
-impl<T> MCasDesc<T> {
-    fn help(&self) {
-        let success = false;
-        for item in self.cases {
-            loop {
-
-            }
-        }
-    }
-}
-
-enum Ptr<T> {
-    MCasDesc(MCasDesc<T>),
-    Ptr(AtomicPtr<T>),
-}
-
-pub(crate) fn m_cas<T>(cases: Vec<Cas<T>>) -> bool {
-    let mut m_cas_desc = MCasDesc {
-        cases,
-        status: Box::new(Status::Undecided),
-    };
-    m_cas_desc.cases.sort();
-    return m_cas_desc.help();
-}
-
-struct CCasDesc<T> {
-    ptr: AtomicPtr<T>,
-    expects: AtomicPtr<T>,
-    new: AtomicPtr<T>,
-    cond: Box<Status>,
-}
-
-pub(crate) fn c_cas<T>(ptr: AtomicPtr<T>, expects: AtomicPtr<T>, new: AtomicPtr<T>, cond: &Status) -> bool {
-    
-}
+pub mod c_cas;
