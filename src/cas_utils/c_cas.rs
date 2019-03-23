4345dabd21fc075bf0d3c3fdc62541c27ffc851d
Yang Keao <keao.yang@yahoo.com>
2019-03-23 15:59:17 +0800

    Fix: logic error in c_cas

--- src/cas_utils/c_cas.rs
+++ src/cas_utils/c_cas.rs
@@ -53,6 +53,16 @@ impl<T> Clone for CCasPtr<T> {
 }
 
 impl<T> CCasPtr<T> {
+    pub fn from_value(val: T) -> CCasPtr<T> {
+        CCasPtr::<T> {
+            inner: Arc::new(UnsafeCell::new(AtomicPtr::new(Box::leak(Box::new(CCasUnion::Value(val))))))
+        }
+    }
+    pub fn from_c_cas_union(union: *mut CCasUnion<T>) -> CCasPtr<T> {
+        CCasPtr::<T> {
+            inner: Arc::new(UnsafeCell::new(AtomicPtr::new(union)))
+        }
+    }
     pub fn c_cas(
         &self,
         expect: *mut CCasUnion<T>,
@@ -76,8 +86,9 @@ impl<T> CCasPtr<T> {
                     desc_ptr,
                     Ordering::SeqCst,
                 ); // TODO: set order carefully
-                if std::ptr::eq(res, desc.borrow_mut_c_cas_desc().expect) {
+                if std::ptr::eq(res, expect_ptr) {
                     desc.borrow_mut_c_cas_desc().help(desc_ptr);
+                    break;
                 } else {
                     match &*res {
                         CCasUnion::CCasDesc(c_cas_desc) => c_cas_desc.help(desc_ptr),
@@ -98,5 +109,27 @@ impl<T> CCasPtr<T> {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_single_c_cas() {
+        let success = Arc::new(AtomicNumLikes::new(Status::Successful));
+        let undecided = Arc::new(AtomicNumLikes::new(Status::Undecided));
+
+        let mut num = CCasUnion::Value(1);
+        let num_ptr = &mut num as *mut CCasUnion<i32>;
+
+        let mut num2 =CCasUnion::Value(2);
+        let num2_ptr = &mut num2 as *mut CCasUnion<i32>;
+
+        let c_cas_ptr = CCasPtr::from_c_cas_union(num_ptr);
+
+        c_cas_ptr.c_cas(num_ptr, num2_ptr, success.clone());
+        assert_eq!(unsafe {*c_cas_ptr.load()}, 1);
     }
 }
